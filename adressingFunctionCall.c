31daa76725862169f7359a2bd6b29f6072598249
senemaktas <30393152+senemaktas@users.noreply.github.com>
2020-09-20 20:27:12 +0300

    Add files via upload

---
+++ adressingFunctionCall.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main(){
+
+   int kare_kenar,kkenar,ukenar,karealan,dikdalan;
+   void alanlar(int *kare_kenar,int *kkenar,int *ukenar,int karealan,int dikdalan);
+
+   printf("sirasiyla kare kanerini, kisa kenari, uzun kenarin degerlerini girin..\n");
+   scanf("%d %d %d", &kare_kenar,&kkenar,&ukenar);
+
+   alanlar(&kare_kenar,&kkenar,&ukenar,&karealan,&dikdalan);
+   printf("karenin alani %d\n",karealan);
+   printf("dikdortgenin alani %d\n",dikdalan);
+
+   return 0;
+}
+
+void alanlar(){
+
+
+
+
+}
+
