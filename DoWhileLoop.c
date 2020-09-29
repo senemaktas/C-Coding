7e26fb9c1d6de36013fa26ae2f10e05b4fda54dd
senemaktas <30393152+senemaktas@users.noreply.github.com>
2020-09-29 22:10:47 +0300

    Create DoWhileLoop.c

---
+++ DoWhileLoop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+int main(){
+
+   int key;
+   int x=;
+   printf("Bir kaarakter giriniz.....\n");
+   
+   do{
+      key=getchar();
+      printf(" Döngüden çıkmak için "z" tuşuna basınız.. girilen karakter = %c ve X değeri = %d  \n"key, x++);
+   
+   } while(key !="z");
+   
+printf(Döngü burada bitti!..);
+
+return 0;
+}
